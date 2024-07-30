ed0f9eca1705177dab1fdeac8530d2f2bf792676
fmoreno98 <franmorenocazadilla@gmail.com>
2024-07-30 17:08:20 +0200

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Footer.css'
+import { Col, Container, Row } from 'react-bootstrap';
+
+function Footer(){
+
+    return(
+        <>
+            
+            <footer>
+                <Container>
+                    <Row style={{"margin" : "10p"}}>
+                        <Col style={{"display": "flex", "justifyContent": "left"}}>
+                            <img style={{width: 40, height:40}} src="./ImagenMiniLogoRedondo.png" />
+                        </Col>
+                        <Col xs={6}>
+                            <p className='text-white'>&copy; 2024 Nombre de la app. Todos los derechos reservados.</p>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
