9df45fc92730cacaeb6a11d1caa56cbc3e310420
Gerard <gbellatriutorres@gmail.com>
2024-08-06 17:03:12 +0200

    Estadísticas sin usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import Register from './components/register'
-import Login from './components/login'
+import Register from './components/Register'
+import Login from './components/Login'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
@@ -9,47 +9,15 @@ import { faBell } from '@fortawesome/free-solid-svg-icons';
 import  './App.css'
 function App() {
 
-  useEffect(() => {
-    const adjustFooterPosition = () => {
-      const footer = document.getElementById('footer');
-      // Obtener la altura del viewport en píxeles
-      const gridBotons = document.getElementById('button-grid');
-      const gridheight = gridBotons.offsetHeight;
-      console.log(gridheight);
-      console.log(screen.height);
-
-      // Comparar si la altura del viewport es mayor que 100vh
-      if (gridheight > 100) {
-        footer.style.position = 'relative';
-      } else {
-        footer.style.position = 'fixed';
-      }
-    };
-
-    // Ejecutar la función al cargar la página
-    adjustFooterPosition();
-    // Añadir el event listener para redimensionar la ventana
-    window.addEventListener('resize', adjustFooterPosition);
-
-    // Cleanup function para eliminar el event listener cuando el componente se desmonte
-    return () => {
-      window.removeEventListener('resize', adjustFooterPosition);
-    };
-  }, []);
-
 
 
   return (
     <>
-    <div className='App'>
       <Header />
-      <div className='content'>
        {/* <Login />
        <Register />     */}
        <Home />
-       </div>
       <Footer />
-      </div>
     </>
   )
 }
