d3880d7b555daafa9adfc7fdc0cdf77161fefb47
Marc Florido <marcfloridoblas12@gmail.com>
2024-08-08 17:44:02 +0200

    formulario editar hecho

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Button, Container, Row, Col, Form } from 'react-bootstrap';
 import BotonesGrid from './BotonesGrid';
+import FormEditar from './FormEditarHabito';
 import Estadisticas from './Estadisticas';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
@@ -11,10 +12,11 @@ const Home = () => {
     return (
         <>
         <Container>
-            <BotonesGrid />
-            <Estadisticas />
+            {/* <BotonesGrid /> */}
+            <FormEditar idHabito={10} />
+            {/* <Estadisticas /> */}
         </Container>
-        <Footer />
+        {/* <Footer /> */}
         </>
         
     );
--- src/components/tools/api.js
+++ src/components/tools/api.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:3000/api';
 
-const login = (email, password) => {
+  const login = (email, password) => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -25,10 +25,47 @@ const login = (email, password) => {
       .catch(error => []);
       
   }
+
+  const buscarHabito = (id) => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    };
+
+    return fetch(API_URL + "/habitos/"+id, requestOptions)
+      .then(response => response.json())
+      .then(r => r.data)
+      .catch(error => console.log(error));
+  } 
+
+  const editarHabito = (nombre_habito, descripcion, tipo_habito, frecuencia, id) => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nombre_habito, descripcion, tipo_habito, frecuencia })
+    };
   
+    return fetch(API_URL + "/habitos/"+id, requestOptions)
+      .then(response => response.json())
+      .catch(error => []);
+  }
+
+  const eliminarHabito = (id) => {
+    const requestOptions = {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({})
+    };
   
+    return fetch(API_URL + "/habitos", requestOptions)
+      .then(response => response.json())
+      .catch(error => []);
+  }
   
   export {
     login,
-    checkToken
+    checkToken,
+    editarHabito,
+    buscarHabito,
+    eliminarHabito
   };
